faf620ff3963258181c288fc96743921030528c8
Wayne Love <wayne@gsjs.org>
2026-08-17 09:44:10 +1000

    Leaf Feature: ZE1 add 12v battery current

--- vehicle/OVMS.V3/components/vehicle_nissanleaf/src/vehicle_nissanleaf.h
+++ vehicle/OVMS.V3/components/vehicle_nissanleaf/src/vehicle_nissanleaf.h
@@ -180,6 +180,7 @@ class OvmsVehicleNissanLeaf : public OvmsVehicle
     void PollReply_Battery(const uint8_t *reply_data, uint16_t reply_len);
     void PollReply_QC(const uint8_t *reply_data, uint16_t reply_len);
     void PollReply_L0L1L2(const uint8_t *reply_data, uint16_t reply_len);
+    void PollReply_12VCurrent(const uint8_t *reply_data, uint16_t reply_len);
     void PollReply_VIN(const uint8_t *reply_data, uint16_t reply_len);
     void PollReply_BMS_Volt(const uint8_t *reply_data, uint16_t reply_len);
     void PollReply_BMS_Shunt(const uint8_t *reply_data, uint16_t reply_len);
